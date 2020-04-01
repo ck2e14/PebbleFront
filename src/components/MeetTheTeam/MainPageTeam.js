ab3d4436c3f75d30845c65501483f64b41ca112e
ck2e14 <chriswkennedy@icloud.com>
2020-04-01 21:28:41 +0100

    copied stateful logic originally in app into MainPageTeam component (soon to be copied into the MISSION and STORIES components) to keep the sidebar responsive. This change is necessary because the sidedrawer no longer functions, as we are now conditionally rendering components from index.js via specific routes as opposed to rendering App.js and working downwards from there. Possible future fix to shift the logic from those three components up into index.js - but this is a functional and low performance-impact method for now.

--- src/components/MeetTheTeam/MainPageTeam.js
+++ src/components/MeetTheTeam/MainPageTeam.js
@@ -2,21 +2,46 @@ import React from 'react';
 import PersonCard from './PersonDisplay/PersonCard';
 import './MainPageTeam.css';    
 import Toolbar from '../Toolbar/Toolbar'; 
+import SideDrawer from '../SideDrawer/SideDrawer';
+import Backdrop from '../Backdrop/Backdrop';
 
 export default class MainPageTeam extends React.Component {
 
+   state = {
+      sideDrawerOpen: false
+    };
+  
+    drawerToggleClickHandler = () => {
+      this.setState((prevState) => {
+        return {sideDrawerOpen: !prevState.sideDrawerOpen}
+      })
+    };
+  
+    backdropClickHandler = () => {
+      this.setState({ sideDrawerOpen: false })
+    }
+
    render(){
+
+      let backdrop;
+      if(this.state.sideDrawerOpen) {
+         backdrop = <Backdrop click={this.backdropClickHandler}/>
+      }
+
       return(
          <>
-            <Toolbar/>
-            <div className='top-div-teampage' style={{position:'fixed', top: '75px', fontSize: '50px'}}> 
+            <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+            <SideDrawer show={this.state.sideDrawerOpen}/>
+            {backdrop}
+               <div className='top-div-teampage' style={{position:'fixed', top: '75px', fontSize: '50px'}}> 
                   <div className='tagline-text'> 
                      Experts in our Field 
                      <span className='tagline-followup-text'> Helping you to achieve the right balance for your business.</span> 
                   </div>
-               <PersonCard/>   
-               <PersonCard/>
-               <PersonCard/>  
+            <PersonCard/>   
+            <PersonCard/>
+            <PersonCard/>  
+
             </div>
          </>
       )
